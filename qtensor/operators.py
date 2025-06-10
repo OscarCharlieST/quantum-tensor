60c33db156879166ee489bd74c7cea1ea3f57536
Oscar Charlie Solomons-Tuke <ucapocs@ucl.ac.uk>
2025-06-10 17:12:50 +0100

    added thermofield hamiltonian maker

--- qtensor/operators.py
+++ qtensor/operators.py
@@ -77,6 +77,47 @@ def tilted_ising(J=1, h=0.25, g=-0.525, N=1):
     r = np.array([0, 0, 1]) # contract with these left and right of the MPO chain
     return uniform_MPO(W, l, r, N)
 
+def thermofield_hamiltonian(H):
+    """
+    Takes a specific form of2 site hamiltonian H
+    where Hl, Hr are the left and right of the two site term
+    and h the 1-site term
+
+    I   Hl  h
+    0   0   Hr
+    0   0   I
+
+    and makes it into a thermofield HxI + IxH via
+
+    I   Hla Hlb ha+hb
+    0   0   0   Hrb
+    0   0   0   Hra
+    0   0   0   I
+
+    There may be a more general perscription, but this is efficient for our hamiltonian
+
+    Note that it only works where the two local terms can be written as tensor product over the two sites. 
+    """
+    H_th = []
+    for i in H.sites:
+        W = H[i]
+        Hl = W[:, :, 0, 1]
+        Hr = W[:, :, 1, 2]
+        h = W[:, :, 0, 2]
+
+        W_th = np.zeros((4, 4, 4, 4), dtype=np.complex64)
+        W_th[:, :, 0, 0] = np.kron(np.eye(2), np.eye(2))
+        W_th[:, :, 0, 1] = np.kron(Hl, np.eye(2))
+        W_th[:, :, 0, 2] = np.kron(np.eye(2), Hl)
+        W_th[:, :, 0, 3] = np.kron(h, np.eye(2)) + np.kron(np.eye(2), h)
+        W_th[:, :, 1, 3] = np.kron(Hr, np.eye(2))
+        W_th[:, :, 2, 3] = np.kron(np.eye(2), Hr)
+        W_th[:, :, 3, 3] = np.kron(np.eye(2), np.eye(2))
+        H_th.append((i, W_th))
+    l = np.array([1, 0, 0, 0])
+    r = np.array([0, 0, 0, 1])
+    return mpo(H_th, l, r)
+
 def norm_operator(N, d):
     W = np.zeros((d, d, 2, 2), dtype=np.complex64)
     W[:, :, 0, 0] = np.eye(d)
